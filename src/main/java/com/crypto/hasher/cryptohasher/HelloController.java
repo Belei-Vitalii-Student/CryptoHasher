2c93b073a81585bf69b77fda5664f532def4691f
vitPrograms <vitalii.beley@gmail.com>
2023-11-01 08:40:46 +0200

    init

---
+++ src/main/java/com/crypto/hasher/cryptohasher/HelloController.java
@@ -0,0 +1,14 @@
+package com.crypto.hasher.cryptohasher;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.crypto.hasher.cryptohasher {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.crypto.hasher.cryptohasher to javafx.fxml;
+    exports com.crypto.hasher.cryptohasher;
+}
